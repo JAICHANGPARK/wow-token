2782e3dab0e5fdb30de3110d7ee73e7e45c5f3cd
Dreamwalker <aristojeff@gmail.com>
2024-12-21 14:06:39 +0900

    Create count_provider.dart

---
+++ lib/providers/count_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+import 'package:jaspr_riverpod/jaspr_riverpod.dart';
+
+/// A provider that preloads its value on the server, and syncs with the client.
+///
+/// The create function of a SyncProvider is only executed on the server, and
+/// never on the client. Instead the synced value is returned.
+final initialCountProvider = SyncProvider<int>((ref) async {
+  return Random().nextInt(100);
+}, id: 'initial_count');
+
+/// A provider that holds the current count.
+///
+/// It uses the synced initial count value as its initial value.
+// final countProvider = StateProvider<int>((ref) {
+//   return ref.watch(initialCountProvider).valueOrNull ?? 0;
+// });
+
+final countProvider = StateProvider((ref)=> 0);
